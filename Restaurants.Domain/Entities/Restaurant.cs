de701e6c10ebe74682e5e29ae96822de6129fee1
Mateusz <matiszczep@gmail.com>
2024-03-16 20:06:50 +0100

    entities creation

--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Restaurants.Domain.Entities
+﻿namespace Restaurants.Domain.Entities
 {
-    internal class Restaurant
+    public class Restaurant
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public Boolean HasDelivery { get; set; }
+
+        public string? ContactEmail { get; set; }
+        public string? ContactNumber { get; set; }
+
+        public Adress? Adress { get; set; }
+        public List<Dish> Dishes { get; set; } = new();
     }
 }
