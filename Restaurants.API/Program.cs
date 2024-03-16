6d14fce3ba46644ecca89a82880de4c7a473c241
Mateusz <matiszczep@gmail.com>
2024-03-16 22:04:28 +0100

    db connection and Entity.Tools addition

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
