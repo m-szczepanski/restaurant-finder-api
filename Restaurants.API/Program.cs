ffeed81e838ab20b813b62ea44f799504d95dea3
Mateusz <matiszczep@gmail.com>
2024-03-18 11:56:10 +0100

    retrieving entities

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,5 +1,7 @@
+using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
+using Restaurants.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
 await seeder.Seed();
 
 // Configure the HTTP request pipeline.
---
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants;
+
+internal class RestaurantsService(IRestaurantRepository restaurantRepository, Logger<RestaurantsService> logger) : IRestaurantsService
+{
+    public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    {
+        logger.LogInformation("Getting all restaurants");
+        var restaurants = await restaurantRepository.GetAllAsync();
+
+        return restaurants;
+    }
+}
+
