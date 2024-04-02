cf95343361f6376bf3835df5df7022830db5aca9
Mateusz <matiszczep@gmail.com>
2024-04-02 12:13:29 +0200

    DishesProfile Profile added

--- Restaurants.Application/Restaurants/DTOs/RestaurantsProfile.cs
+++ Restaurants.Application/Restaurants/DTOs/RestaurantsProfile.cs
@@ -1,6 +1,22 @@
-﻿namespace Restaurants.Application.Restaurants.DTOs;
+﻿using AutoMapper;
+using Restaurants.Application.Dishes.DTOs;
+using Restaurants.Domain.Entities;
 
-public class RestaurantsProfile
+namespace Restaurants.Application.Restaurants.DTOs;
+
+public class RestaurantsProfile : Profile
 {
-}
+    public RestaurantsProfile()
+    {
+        CreateMap<Restaurant, RestaurantDto>()
+            .ForMember(d => d.City, o => 
+                o.MapFrom(s => s.Address == null ? null : s.Address.City))
+            .ForMember(d => d.Street, o => 
+                o.MapFrom(s => s.Address == null ? null : s.Address.Street))
+            .ForMember(d => d.PostalCode, o => 
+                o.MapFrom(s => s.Address == null ? null : s.Address.PostalCode))
+            .ForMember(d => d.Dishes, o => 
+                o.MapFrom(s => s.Dishes.Select(DishDto.FromEntity).ToList()));
+    }
+}   
 
