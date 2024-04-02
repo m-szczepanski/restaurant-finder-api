02af836a42cddeb53f2f11b1aa166b2a8053a908
Mateusz <matiszczep@gmail.com>
2024-04-02 13:07:09 +0200

    deleted previous logic

--- Restaurants.Application/Dishes/DTOs/DishDto.cs
+++ Restaurants.Application/Dishes/DTOs/DishDto.cs
@@ -11,17 +11,5 @@ public class DishDto
     public int? KiloCalories { get; set; }
 
     public int RestaurantId { get; set; }
-
-    public static DishDto FromEntity(Dish dish)
-    {
-        return new DishDto
-        {
-            Id = dish.Id,
-            Name = dish.Name,
-            Description = dish.Description,
-            Price = dish.Price,
-            KiloCalories = dish.KiloCalories
-        };
-    }
 }
 
--- Restaurants.Application/Restaurants/DTOs/RestaurantDto.cs
+++ Restaurants.Application/Restaurants/DTOs/RestaurantDto.cs
@@ -17,23 +17,5 @@ public class RestaurantDto
 
 
     public List<DishDto> Dishes { get; set; } = [];
-
-    public static RestaurantDto? FromEntity(Restaurant? restaurant)
-    {
-        if (restaurant is null) return null;
-
-        return new RestaurantDto()
-        {
-            Category = restaurant.Category,
-            Description = restaurant.Description,
-            Id = restaurant.Id,
-            HasDelivery = restaurant.HasDelivery,
-            Name = restaurant.Name,
-            City = restaurant.Address?.City,
-            Street = restaurant.Address?.Street,
-            PostalCode = restaurant.Address?.PostalCode,
-            Dishes = restaurant.Dishes.Select(DishDto.FromEntity).ToList()
-        };
-    }
 }
 
