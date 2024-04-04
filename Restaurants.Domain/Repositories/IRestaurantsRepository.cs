527210dfceffcad0913a2b741ae3777a2fdc5c82
Mateusz <matiszczep@gmail.com>
2024-04-04 18:48:47 +0200

    deleting resources

--- Restaurants.Domain/Repositories/IRestaurantsRepository.cs
+++ Restaurants.Domain/Repositories/IRestaurantsRepository.cs
@@ -7,5 +7,6 @@ public interface IRestaurantsRepository
     Task<IEnumerable<Restaurant>> GetAllAsync();
     Task<Restaurant?> GetByIdAsync(int id);
     Task<int> Create(Restaurant restaurant);
+    Task Delete(Restaurant entity);
 }
 
