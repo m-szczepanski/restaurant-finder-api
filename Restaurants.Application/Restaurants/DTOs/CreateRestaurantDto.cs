96b407811913e8ab300dba2e66632a2415134ec9
Mateusz <matiszczep@gmail.com>
2024-04-04 14:18:52 +0200

    fluent validation

--- Restaurants.Application/Restaurants/DTOs/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/DTOs/CreateRestaurantDto.cs
@@ -4,21 +4,18 @@ namespace Restaurants.Application.Restaurants.DTOs;
 
 public class CreateRestaurantDto
 {
-    [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
-    [Required(ErrorMessage = "Insert a valid category")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
-    [EmailAddress(ErrorMessage = "Insert a valid email")]
     public string? ContactEmail { get; set; }
     [Phone(ErrorMessage = "Insert a valid phone number")]
     public string? ContactNumber { get; set; }
 
     public string? City { get; set; }
     public string? Street { get; set; }
-    [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Provide a valid postal code (XX-XXX)")]
+
     public string? PostalCode { get; set; }
 }
 
